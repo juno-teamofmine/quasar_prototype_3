c5a554902b85947fbddda4133f5c7dd45113478e
juno <selectjuno@teamofmine.com>
2021-06-07 11:27:28 +0900

    커밋 누락건

---
+++ src/pages/native/FileSystem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <q-btn color="primary" label="home" @click="back" />
+  </div>
+
+  <q-file filled v-model="fileModel" label="Label (stacked)" stack-label />
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      fileModel: ref(null)
+    }
+  },
+  methods: {
+    back() {
+      this.$router.back();
+    }
+  }
+}
+</script>
